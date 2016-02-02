ce077a4e12a3864881a5e03ed28e0b78f0c771de
Rhys Short <rhyshort@uk.ibm.com>
2016-02-02 14:40:53 +0000

    Revert header changes

--- Unirest/UNIHTTPClientHelper.h
+++ Unirest/UNIHTTPClientHelper.h
@@ -23,10 +23,10 @@
  WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
-#import "UNIHTTPResponse.h"
+#import "HttpResponse/UNIHTTPResponse.h"
 #import "UNIHTTPRequest.h"
 #import "UNIHTTPRequestWithBody.h"
-#import "UNISimpleRequest.h"
+#import "HttpRequest/UNISimpleRequest.h"
 #import "UNIUrlConnection.h"
 
 #define BOUNDARY @"---------------------------17237809831461299884346131229"
